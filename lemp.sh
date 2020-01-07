5be7c7dd164b7728ef2e22fab92a5e9233817e01
lemp-info <59017755+lemp-info@users.noreply.github.com>
2020-01-07 23:18:30 +0300

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -191,7 +191,8 @@ if [[ $osrelease  = "7."* ]] ; then
 yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-5.9.5-3.el7.x86_64.rpm -y
 yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-devel-5.9.5-3.el7.x86_64.rpm -y
 fi
-if [[ $osrelease  = "7."* ]] ; then 
+if [[ $osrelease  = "8."* ]] ; then 
+yum install http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/oniguruma-6.8.2-1.el8.x86_64.rpm -y
 fi
 
 wget https://sourceforge.net/projects/lemp-info/files/lempCentOS.tar.gz -P /home/lemp/ 
@@ -205,14 +206,12 @@ cp /home/lemp/script/mysql.server /etc/rc.d/init.d/
 mv /home/lemp/script/* /etc/rc.d/rc0.d/  
 
 if [[ $osrelease  = "8."* ]] ; then 
-yum install http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/oniguruma-6.8.2-1.el8.x86_64.rpm -y
 rm -rf /home/lemp/php
 wget  https://sourceforge.net/projects/lemp-info/files/lempCentOS8PHP741.gz -P /home/lemp/ 
 sudo chmod -R 755 /home/lemp/lempCentOS8PHP741.tar.gz
 tar -xvzf /home/lemp/lempCentOS8PHP741.gz -C /home/lemp/
 rm -rf /home/lemp/lempCentOS8PHP741.gz
 fi
-
 fi
 
 if [ $MariaDB = "y" ];then
@@ -249,15 +248,12 @@ sudo /home/lemp/mysql/bin/mysqladmin -u root password "$SQL"
 sleep 1
 mysql -uroot -p"$SQL" -e "CREATE DATABASE phpmyadmin"  
 mysql -uroot -p"$SQL" phpmyadmin < /home/lemp/phpmyadmin/phpmyadmin.sql 
-
 fi
 
 sudo chmod 775 /home/lemp/phpmyadmin/tmp
 sudo chmod 777 /home/lemp/phpmyadmin/tmp
 
 if [ "$osname" == "Ubuntu" ]; then
- 
-
 if [ -f "$file" ]
 then
 sed --in-place '/exit 0/d' /etc/rc.local 
