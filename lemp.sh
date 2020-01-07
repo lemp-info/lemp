32851643e3848e0b270b8343880f69e2b83e0c14
lemp-info <59017755+lemp-info@users.noreply.github.com>
2020-01-07 20:41:30 +0300

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -192,10 +192,7 @@ yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-
 yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-devel-5.9.5-3.el7.x86_64.rpm -y
 fi
 if [[ $osrelease  = "7."* ]] ; then 
-yum install http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/oniguruma-6.8.2-1.el8.x86_64.rpm -y
 fi
-
-
 
 wget https://sourceforge.net/projects/lemp-info/files/lempCentOS.tar.gz -P /home/lemp/ 
 tar -xvzf  /home/lemp/lempCentOS.tar.gz -C /home/lemp
@@ -208,6 +205,7 @@ cp /home/lemp/script/mysql.server /etc/rc.d/init.d/
 mv /home/lemp/script/* /etc/rc.d/rc0.d/  
 
 if [[ $osrelease  = "8."* ]] ; then 
+yum install http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/oniguruma-6.8.2-1.el8.x86_64.rpm -y
 rm -rf /home/lemp/php
 wget  https://sourceforge.net/projects/lemp-info/files/lempCentOS8PHP741.gz -P /home/lemp/ 
 sudo chmod -R 755 /home/lemp/lempCentOS8PHP741.tar.gz
