7b32818d0bcc91596a8f89bfc316c4332c90ee76
lemp-info <59017755+lemp-info@users.noreply.github.com>
2020-03-25 10:17:16 +0300

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -199,11 +199,11 @@ yum install -y sqlite-devel.x86_64
 yum install -y systemd-devel 
 #yum install -y iptables-services
 if [[ $osrelease  = "7."* ]] ; then 
-yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-5.9.5-3.el7.x86_64.rpm -y
-yum install https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-devel-5.9.5-3.el7.x86_64.rpm -y
+yum install https://github.com/lemp-info/lempNew/raw/master/oniguruma-5.9.5-3.el7.x86_64.rpm -y
+yum install https://github.com/lemp-info/lempNew/raw/master/oniguruma-devel-5.9.5-3.el7.x86_64.rpm -y
 fi
 if [[ $osrelease  = "8."* ]] ; then 
-yum install http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/oniguruma-6.8.2-1.el8.x86_64.rpm -y
+yum install https://github.com/lemp-info/lempNew/raw/master/oniguruma-6.8.2-1.el8.x86_64.rpm -y
 fi
 
 wget https://sourceforge.net/projects/lemp-info/files/lempCentOS.tar.gz -P /home/lemp/ 
