1612de42c1860e4d4438c91502b244180053ce0f
lemp-info <59017755+lemp-info@users.noreply.github.com>
2020-12-08 09:37:38 -0800

    Update MySQL-5.5.62.sh

--- MySQL-5.5.62.sh
+++ MySQL-5.5.62.sh
@@ -66,10 +66,12 @@ fi
 sudo apt-get remove --purge mysql* -y
 sleep 1
 sudo apt-get purge mysql* -y
+rm -rf /usr/local/mysql
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sudo apt-get remove dbconfig-mysql -y
 #sudo apt-get dist-upgrade -y  
+
 else
   echo " "
 fi
