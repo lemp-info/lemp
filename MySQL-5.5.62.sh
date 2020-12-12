f2352e8987ba49c597026a3d6fc74144631b6cfe
lemp-info <59017755+lemp-info@users.noreply.github.com>
2020-12-11 23:53:19 -0800

    Update MySQL-5.5.62.sh

--- MySQL-5.5.62.sh
+++ MySQL-5.5.62.sh
@@ -67,6 +67,7 @@ sudo apt-get remove --purge mysql* -y
 sleep 1
 sudo apt-get purge mysql* -y
 rm -rf /usr/local/mysql
+rm -rf /home/lemp/mysql
 rm -rf /usr/local/bin/mysql*
 rm -rf /usr/local/bin/innochecksum  
 rm -rf /usr/local/bin/myisam_ftdump 
@@ -80,6 +81,8 @@ rm -rf /usr/local/bin/myisamchk
 rm -rf /usr/local/bin/myisampack  
 rm -rf /usr/local/bin/replace
 rm -rf /etc/my.cnf
+rm -rf /etc/init.d/mysql.server
+rm -rf /etc/init.d/my.cnf
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sudo apt-get remove dbconfig-mysql -y
