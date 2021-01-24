ef631d46e5e76b2fdcbc694560ebce264ac8a726
lemp-info <59017755+lemp-info@users.noreply.github.com>
2021-01-24 08:33:50 +0300

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -180,6 +180,7 @@ rm -r *.deb
 rm -r *.rpm 
 sudo ln -s /usr/lib64/libpng12.so.0 /usr/lib/x86_64-linux-gnu/libpng12.so.0
 fi
+sleep 1
 while true; do
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=1dHIwBLT_-YaMOwdiyoQtewdMELh_QnV3" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dHIwBLT_-YaMOwdiyoQtewdMELh_QnV3" -O /home/lemp/lempNEW.tar.gz  && rm -rf /tmp/cookies.txt
 sleep 1
@@ -442,7 +443,9 @@ mysql -uroot -p"$mysqlpassword" phpmyadmin < /home/lemp/phpmyadmin/phpmyadmin.sq
 fi 
 fi
 fi
-
+chown lemp:lemp /home/lemp
+chown lemp:lemp /home/lemp/*
+chown -R mysql.mysql /home/lemp/mysql
 if (whiptail --title "Restart." --yesno "Do you want to restart now ?" 8 78); then   
 reboot
 else
